b6f7d61212858c6fd70b6e125fb63573ddb1b97c
3m1n3nc3 <mygames.ng@gmail.com>
2025-08-23 22:14:35 +0100

    feat: add dump-die to framework support package.

---
+++ packages/support/src/Helpers/DumpDie.ts
@@ -0,0 +1,18 @@
+import process from 'process'
+import util from 'util'
+
+const inspect = (thing: any) => {
+    return util.inspect(thing, {
+        showHidden: true,
+        depth: null,
+        colors: true
+    })
+}
+
+export const dd = (...args: unknown[]): never => {
+    args.forEach((thing) => {
+        console.log(inspect(thing))
+    })
+
+    process.exit(1)
+} 
