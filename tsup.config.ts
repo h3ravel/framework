f83d06b93e906c7787130e6e8bf5921ba4532c7a
3m1n3nc3 <mygames.ng@gmail.com>
2025-08-24 01:29:59 +0100

    feat: copy .d.ts files from packages to dist dir after installation.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,21 @@
+import { copyFile, glob } from 'node:fs/promises'
+
 import { defineConfig } from 'tsup'
+import escalade from 'escalade/sync'
+import { existsSync } from 'node:fs'
+import path from 'node:path'
+
+function findUpConfig (base: string, name: string, extensions: string[]) {
+    return escalade(process.cwd(), (_dir) => {
+        for (const ext of extensions) {
+            const filename = path.join(_dir, base, name + '.' + ext)
+            if (existsSync(filename)) {
+                return filename
+            }
+        }
+        return ''
+    })!
+}
 
 export default defineConfig({
     dts: true,
@@ -6,6 +23,13 @@ export default defineConfig({
     entry: ['src/index.ts'],
     format: ['esm', 'cjs'],
     sourcemap: true,
+    async onSuccess () {
+        const base = findUpConfig('framework', 'package', ['json'])
+        const ptrn = base.replace('package.json', 'packages/**/src/*.d.ts')
+        for await (const entry of glob(ptrn))
+            setTimeout(() => copyFile(entry, entry.replace('src', 'dist')), 3000)
+
+    },
     external: [
         'fs',
         'path',
