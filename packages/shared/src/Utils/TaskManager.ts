4575c79d9eea19a2fc32e1dca43165945747d19b
3m1n3nc3 <mygames.ng@gmail.com>
2025-09-22 11:59:17 +0100

    feat: create TaskManagerClass

---
+++ packages/shared/src/Utils/TaskManager.ts
@@ -0,0 +1,25 @@
+import { Logger } from "./Logger"
+
+export class TaskManager {
+    public static async taskRunner (
+        description: string,
+        task: (() => Promise<any>) | (() => any)
+    ): Promise<void> {
+        const startTime = process.hrtime()
+        let result: any = false
+
+        try {
+            result = await Promise.all([(task || (() => true))()].flat())
+        } finally {
+            const endTime = process.hrtime(startTime)
+            const duration = (endTime[0] * 1e9 + endTime[1]) / 1e6
+            Logger.twoColumnLog(
+                Logger.parse([[description, 'green']], '', false),
+                [
+                    Logger.parse([[`${Math.floor(duration)}ms`, 'gray']], '', false),
+                    Logger.parse([[result !== false ? '✔' : '✘', result !== false ? 'green' : 'red']], '', false),
+                ].join(' ')
+            )
+        }
+    }
+}
