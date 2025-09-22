2b7d23518ce8875e08c92ccfc362c2a19eb68999
3m1n3nc3 <mygames.ng@gmail.com>
2025-09-22 16:47:12 +0100

    chore: cleanup

--- jest.config.ts
+++ jest.config.ts
@@ -19,6 +19,7 @@ const jestConfig: JestConfigWithTsJest = {
     '@h3ravel/router': '<rootDir>/packages/router/src/index.ts',
     '@h3ravel/http': '<rootDir>/packages/http/src/index.ts',
     '@h3ravel/config': '<rootDir>/packages/config/src/index.ts',
+    '@h3ravel/filesystem': '<rootDir>/packages/filesystem/src/index.ts',
     '@h3ravel/database': '<rootDir>/packages/database/src/index.ts',
     '@h3ravel/cache': '<rootDir>/packages/cache/src/index.ts',
     '@h3ravel/mail': '<rootDir>/packages/mail/src/index.ts',
