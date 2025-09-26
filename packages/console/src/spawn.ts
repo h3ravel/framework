7de63080fa914ce63f678cfdf060711a9f20a19e
3m1n3nc3 <mygames.ng@gmail.com>
2025-09-26 02:24:35 +0100

    feat: add a span script to quickly build the project.

---
+++ packages/console/src/spawn.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { execa } from 'execa'
+import { join } from 'node:path'
+import preferredPM from 'preferred-pm'
+
+const build = async () => {
+    const pm = (await preferredPM(process.cwd()))?.name ?? 'npm'
+    const outDir = join(process.env.DIST_DIR ?? '.h3ravel/serve')
+
+    const ENV_VARS = {
+        EXTENDED_DEBUG: 'false',
+        CLI_BUILD: 'true',
+        NODE_ENV: 'production',
+        DIST_DIR: outDir,
+        LOG_LEVEL: 'silent'
+    }
+
+    await execa(
+        pm,
+        ['tsdown', '--silent', '--config-loader', 'unconfig', '-c', 'tsdown.default.config.ts'].filter(e => e !== null),
+        { stdout: 'inherit', stderr: 'inherit', cwd: join(process.cwd()), env: Object.assign({}, process.env, ENV_VARS) }
+    )
+}
+
+build()
