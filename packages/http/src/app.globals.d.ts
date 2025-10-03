ee098566958884a81d9cf94ff3c8c35b1b2a9ccf
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2025-10-03 15:45:44 +0100

    Update app.globals.d.ts

--- packages/http/src/app.globals.d.ts
+++ packages/http/src/app.globals.d.ts
@@ -1,1 +1,13 @@
+import { Url } from './Url'
 
+export { }
+
+declare global {
+    /**
+     * Generate URLs or get the Url instance
+     *
+     * @param path
+     */
+    function url (): Url
+    function url (path: string): string
+}
