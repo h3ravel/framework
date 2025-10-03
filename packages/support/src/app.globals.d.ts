ede495b0547fde4021c8b45ab817b98d7b0ddc32
3m1n3nc3 <mygames.ng@gmail.com>
2025-10-03 15:43:27 +0100

    update: update helper comment

--- packages/support/src/app.globals.d.ts
+++ packages/support/src/app.globals.d.ts
@@ -4,9 +4,9 @@ export { }
 
 declare global {
     /**
-     * Dump something and kill the process for quick debugging. Based on Laravel's dd()
+     * Get a new Stringable object from the given string.
      * 
-     * @param args 
+     * @param string 
      */
     function str (string?: string): Stringable
 }
